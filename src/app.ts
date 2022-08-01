39cb1049ddeeb554e4d23d6d5a62d264916964a2
Mayara <mayandronico@msn.com>
2022-08-01 15:18:15 -0300

    Requisito 2 e middleware de erro

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import errorHandler from './middlewares/error';
+import 'express-async-errors';
 
 const app = express();
+app.use(express.json());
+app.use(errorHandler);
 
 export default app;
