f437bff59076c95ef89995fce3c7ef0dfb4a153b
Mayara <mayandronico@msn.com>
2022-08-01 15:32:11 -0300

    Requisito 3

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+const carZodSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().gte(1900).lte(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
+  doorsQty: z.number().gte(2).lte(4),
+  seatsQty: z.number().gte(2).lte(7),
+});
+
+type ICar = z.infer<typeof carZodSchema>;
+
+export default ICar;
+export { ICar, carZodSchema };
